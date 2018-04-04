f331b217e2bb7e58d763bad049b27c447bf36be9
godruoyi <godruoyixu@dayuw.com>
2018-04-04 18:00:10 +0800

    Vue router scroll behavior

--- resources/assets/js/frontend/router/index.js
+++ resources/assets/js/frontend/router/index.js
@@ -8,6 +8,13 @@ const router = new Router({
     routes,
     linkActiveClass: '',
     mode: 'history', // do not use /#/.
+    scrollBehavior: function (to, from, savedPosition) {
+        if (to.hash) {
+            return {selector: to.hash}
+        }
+
+        return savedPosition ? savedPosition : {x: 0, y: 0}
+    }
 })
 
 export default router
