815d46b4078eefdd0777aada95b17a8b06a3e586
Xqilu <155749009+QL776@users.noreply.github.com>
2025-01-18 18:02:24 +0800

    修改一点内容

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ import type { NavigationLink, Site } from './types.ts'
 
 export const SITE: Site = {
     author: 'Godruoyi',
-    url: 'https://godruoyi.com',
-    title: '连波的闲谈杂鱼',
+    url: 'https://76log.pages.dev/',
+    title: '776\'s log',
     description: 'Godruoyi\'s personal blog, I enjoy the process of building something using any technology stack',
     shortDescription: '',
 }
