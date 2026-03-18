0e974f67fa6d034bac6979283e5420c1af6e7e3e
Godruoyi <godruoyi@gmail.com>
2026-03-18 17:25:22 +0800

    add image for life category

--- src/content/config.ts
+++ src/content/config.ts
@@ -34,6 +34,7 @@ const categoryCollection = defineCollection({
     schema: () => z.object({
         title: z.string(),
         description: z.string(),
+        background: z.string().optional(),
     }),
 })
 
